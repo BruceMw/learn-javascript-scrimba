d7cf922f284ed4f577aaa608a25bcb03942db9d5
Bruce <43759619+BruceMw@users.noreply.github.com>
2022-05-05 18:27:10 +0200

    start again 🎉

--- 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
+++ 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -8,6 +8,10 @@
 
 
 // 1. Create two variables, myAge and humanDogRatio
+let dogAge = 35;
+let humanDogRatio = 7;
 // 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = dogAge * humanDogRatio;
 // 3. Log myDogAge to the console
 
+console.log(myDogAge);
--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -5,6 +5,7 @@ let count = 5
 console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bone
 // Decrease it down to 25, and then finally increase it to 70
 
 
